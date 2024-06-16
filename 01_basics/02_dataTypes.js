a04b5c3d07eecba0af3f36990305a2a28d628c78
NamishV117 <119800049+NamishV117@users.noreply.github.com>
2024-06-16 07:18:50 +0000

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,17 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3) //This is doesn't work as we are using Nodejs but not Web broswer
+
+let Name = "Namish"
+let age = 19
+
+//Primitive Data types
+/*
+number => 2^53
+bigint
+string
+boolean
+null
+undefined
+symbol => unique*/
+
